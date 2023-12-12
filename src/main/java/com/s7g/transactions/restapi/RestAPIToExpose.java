9556557c7bbea903fa761c625155c0cf244e4be3
SharyuDock <sharyudata1@gmail.com>
2023-12-12 18:12:17 +0530

    test

---
+++ src/main/java/com/s7g/transactions/restapi/RestAPIToExpose.java
@@ -0,0 +1,15 @@
+package com.s7g.transactions.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestAPIToExpose {
+
+    @GetMapping("/data")
+    public String getData() {
+        return "data";
+    }
+}
---
+++ src/test/java/com/s7g/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s7g.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
